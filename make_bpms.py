0d545369fec68377f43ecad39308e1226f3e9baa
DLS Student Temp <DLSSTUDENTTEMP@FED.CCLRC.AC.UK>
2017-07-06 17:09:10 +0100

    added pvs to all the widgets and changed them to leds

--- make_bpms.py
+++ make_bpms.py
@@ -2,71 +2,15 @@ from opimodel import colors, fonts, rules, widgets
 from renderers.css import render
 import os
 
-#each green rectangle
-def green_rectangle (x,y):
-	#top right square
-	w = widgets.Rectangle(x+10, y+5, 11, 11)
-	w.set_bg_color(colors.GREEN)
-	w.line_color = colors.BLACK
-	w.line_width = 1
-	w.transparent = False
-	d.add_child(w)
-
-	#top left square
-	w = widgets.Rectangle(x, y+5, 11, 11)
-	w.set_bg_color(colors.GREEN)
-	w.line_color = colors.BLACK
-	w.line_width = 1
-	w.transparent = False
-	d.add_child(w)
-
-
-	#mini rectangle
-	w = widgets.Rectangle(x, y, 21, 6)
-	w.set_bg_color(colors.GREEN)
-	w.line_color = colors.BLACK
-	w.line_width = 1
-	w.transparent = False
-	d.add_child(w)
-
-	#bottom right square
-	w = widgets.Rectangle(x+10, y+15, 11, 11)
-	w.set_bg_color(colors.GREEN)
-	w.line_color = colors.BLACK
-	w.line_width = 1
-	w.transparent = False
-	d.add_child(w)
-
-	#bottom left square
-	w = widgets.Rectangle(x, y+15, 11, 11)
-	w.set_bg_color(colors.GREEN)
-	w.line_color = colors.BLACK
-	w.line_width = 1
-	w.transparent = False
-	d.add_child(w)
-
 # Read the color and font data CSStudio configuration files
 example_dir = os.path.dirname(os.path.realpath(__file__))
 colors.parse_css_color_file(os.path.join(example_dir, 'examples', 'color.def'))
 fonts.parse_css_font_file(os.path.join(example_dir, 'examples', 'font.def'))
 
+
 # root widgetz
 d = widgets.Display(604, 367)
 d.set_bg_color(colors.CANVAS)
-
-#most of the green rectangles
-for x in range(24):
-	for y in range(7):
-		green_rectangle(28+24*x, 135+29*y)
-		
-#isolated green rectangle 
-green_rectangle(52,338)
-
-#other four isolatedish green rectangles
-green_rectangle(220,77)
-green_rectangle(316,77)
-green_rectangle(220,106)
-green_rectangle(316,106)
 
 #horizontal grey squares
 
@@ -98,6 +42,91 @@ for i in range(8):
 	d.add_child(l)
 	l.set_bg_color(colors.GREY_50_)
 	l.transparent = False
+#each green rectangle
+def green_rectangle (x,y,pv_prefix,pv_prefix2):
+	#top right square
+	w = widgets.Led(x+10, y+5, 11, 11,pv_prefix.format("V")+"SLOW:DISABLED")
+	w.set_bg_color(colors.GREEN)
+	w.transparent = False
+	w.square_led = True
+	w.effect_3d = False
+	w.border_alarm_sensitive = False
+	w.bulb_border_color = colors.BLACK
+	w.off_color = colors.GREEN_LED_ON
+	w.on_color = colors.GREEN_LED_OFF
+	w.bulb_border = '1'
+	d.add_child(w)
+
+	#top left square
+	w = widgets.Led(x, y+5, 11, 11,pv_prefix.format("H")+"SLOW:DISABLED")
+	w.set_bg_color(colors.GREEN)
+	w.transparent = False
+	w.square_led = True
+	w.effect_3d = False
+	w.border_alarm_sensitive = False
+	w.bulb_border_color = colors.BLACK
+	w.off_color = colors.GREEN_LED_ON
+	w.on_color = colors.GREEN_LED_OFF
+	w.bulb_border = '1'
+	d.add_child(w)
+
+
+	#mini rectangle
+	w = widgets.Led(x, y, 21, 6,pv_prefix2+"CF:ENABLED_S")
+	w.set_bg_color(colors.GREEN)
+	w.transparent = False
+	w.square_led = True
+	w.effect_3d = False
+	w.border_alarm_sensitive = False
+	w.bulb_border_color = colors.BLACK
+	w.off_color = colors.GREEN_LED_OFF
+	w.on_color = colors.GREEN_LED_ON
+	w.bulb_border = '1'
+	d.add_child(w)
+
+	#bottom right square
+	w = widgets.Led(x+10, y+15, 11, 11, pv_prefix.format("V")+"FAST:DISABLED")
+	w.set_bg_color(colors.GREEN)
+	w.transparent = False
+	w.square_led = True
+	w.effect_3d = False
+	w.border_alarm_sensitive = False
+	w.bulb_border_color = colors.BLACK
+	w.off_color = colors.GREEN_LED_ON
+	w.on_color = colors.GREEN_LED_OFF
+	w.bulb_border = '1'
+	d.add_child(w)
+
+	#bottom left square
+	w = widgets.Led(x, y+15, 11, 11, pv_prefix.format("H")+"FAST:DISABLED")
+	w.set_bg_color(colors.GREEN)
+	w.transparent = False
+	w.square_led = True
+	w.effect_3d = False
+	w.border_alarm_sensitive = False
+	w.bulb_border_color = colors.BLACK
+	w.off_color = colors.GREEN_LED_ON
+	w.on_color = colors.GREEN_LED_OFF
+	w.bulb_border = '1'
+	d.add_child(w)
+
+
+#most of the green rectangles
+for x in range(24):
+	for y in range(7):
+		green_rectangle(28+24*x, 135+29*y, "SR{:02d}C-PC-{}BPM-{:02d}:".format(x+1,"{}",y+1),"SR{:02d}C-DI-EBPM-{:02d}:".format(x+1,y+1 ))
+
+
+#isolated green rectangle 
+green_rectangle(52,338,"SR02C-PC-{}BPM-08:","SR02C-DI-EBPM-08:")
+
+#other four isolatedish green rectangles
+green_rectangle(220,77,"SR09S-PC-{}BPM-01:","SR09S-DI-EBPM-01:")
+green_rectangle(316,77,"SR13S-PC-{}BPM-01:","SR13S-DI-EBPM-01:")
+green_rectangle(220,106,"SR09S-PC-{}BPM-02:","SR09S-DI-EBPM-02:")
+green_rectangle(316,106,"SR09S-PC-{}BPM-02:","SR13S-DI-EBPM-02:")
+
+
 
 #textbox
 l = widgets.Label(354, 80, 164, 32, "All globally enabled BPMs\n are used by RF feedback")
